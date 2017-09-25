298fbe40ff89df2fab53e30db0ceb904a4cbabd0
Gil Akos <gil@astra.finance>
2017-09-25 15:57:28 -0500

    Feat: Section 1 Complete

--- Term 2/P2_RNN-Text-Generation/my_answers.py
+++ Term 2/P2_RNN-Text-Generation/my_answers.py
@@ -3,6 +3,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Activation
 import keras
 
 
@@ -12,7 +13,18 @@ def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
+    
+    # ensure series is np array
+    data_np = np.array(series)
+    
+    # loop through the items in the series
+    for index in range(len(data_np) - window_size):
+        window = data_np[index:index+window_size]
+        target = data_np[index+window_size]
+        X.append(window)
+        y.append(target)
 
+    
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -23,7 +35,13 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size,1)))
+    model.add(Dense(1))
+    model.add(Activation('linear'))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
+
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
